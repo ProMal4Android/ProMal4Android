97d5f3afe61d8af81c1575d819956f5a02669b76
ProMal4Android <158020802+ProMal4Android@users.noreply.github.com>
2024-04-18 20:36:18 +0800

    Delete verify directory

--- verify/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from verify.views import mainTask,verifyByFeature,verifyByNode
-
-urlpatterns=[
-    path('test0', mainTask),
-    path('test1', verifyByFeature),
-    path('test2', verifyByNode),
-]
